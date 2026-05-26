0ce0da6e13f5125e1784f255867a12f7826b1c35
Jacob Quaye <kofimave@gmail.com>
2026-05-26 08:57:17 +0000

    feat: migrate infrastructure from ECR to ECS Fargate with a new CloudFormation stack

---
+++ src/main/java/org/mave/containerization_demo/LabController.java
@@ -0,0 +1,30 @@
+package org.mave.containerization_demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class LabController {
+
+    @GetMapping("/lab")
+    public String labForm(Model model) {
+        model.addAttribute("fullName", "");
+        model.addAttribute("labName", "");
+        model.addAttribute("submitted", false);
+        return "lab";
+    }
+
+    @PostMapping("/lab")
+    public String labSubmit(
+            @RequestParam String fullName,
+            @RequestParam String labName,
+            Model model) {
+        model.addAttribute("fullName", fullName);
+        model.addAttribute("labName", labName);
+        model.addAttribute("submitted", true);
+        return "lab";
+    }
+}
