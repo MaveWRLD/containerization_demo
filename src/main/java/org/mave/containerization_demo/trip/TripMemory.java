b0713775134a4185552c4be274efabf3d2e9e062
Jacob Quaye <kofimave@gmail.com>
2026-07-04 20:29:00 +0000

    refactor: replace `title`, `destination`, and `notes` with `description` in trip memory feature templates and model

--- src/main/java/org/mave/containerization_demo/trip/TripMemory.java
+++ src/main/java/org/mave/containerization_demo/trip/TripMemory.java
@@ -1,26 +1,21 @@
 package org.mave.containerization_demo.trip;
 
 import jakarta.persistence.*;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-import java.time.LocalDate;
+import lombok.*;
 
 @Entity
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class TripMemory {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String title;
-    private String destination;
-    private LocalDate tripDate;
-
     @Column(length = 2000)
-    private String notes;
+    private String description;
 
     private String imagePath;
 }
