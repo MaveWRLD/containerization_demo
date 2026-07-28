bd894d1637f487addcf25a1458759143920332f8
Jacob Quaye <kofimave@gmail.com>
2026-07-28 09:14:43 +0000

    ecs-change

--- src/main/java/org/mave/containerization_demo/LabController.java
+++ src/main/java/org/mave/containerization_demo/LabController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/")
 public class LabController {
 
-    @GetMapping("/lab")
+    @GetMapping("/ecs-change")
     public String labForm() {
         return "lab";
     }
